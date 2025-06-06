0129be9fde1a2e8a03b017e6a3620a4709248b85
simon-templer <simon.kai.schmalz@gmail.com>
2025-06-06 16:08:29 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-8">
+      <Button variant="default">shadcn/ui Button</Button>
+    </main>
+  );
+}
